5caf6692502ddd2eb94af5779c2416d70d531fe1
danis <danis.cavkic0209@gmail.com>
2025-12-09 19:54:44 +0100

    Update session data and constants, remove old token logs

--- src/constants.rs
+++ src/constants.rs
@@ -39,22 +39,22 @@ pub const JITO_ENDPOINTS: [&str; 4] = [
 ];
 
 /// Helius tip accounts (required: minimum 200,000 lamports tip)
-pub const HELIUS_TIP_ACCOUNTS: [&str; 11] = [
+pub const HELIUS_TIP_ACCOUNTS: [&str; 10] = [
+    "4ACfpUFoaSD9bfPdeu6DBt89gB6ENTeHBXCAi87NhDEE",
     "D2L6yPZ2FmmmTKPgzaMKdhu6EWZcTpLy1Vhx8uvZe7NZ",
+    "9bnz4RShgq1hAnLnZbP8kbgBg1kEmcJBYQq3gQbmnSta",
     "5VY91ws6B2hMmBFRsXkoAAdsPHBJwRfBht4DXox3xkwn",
     "2nyhqdwKcJZR2vcqCyrYsaPVdAnFoJjiksCXJ7hfEYgD",
     "2q5pghRs6arqVjRvT5gfgWfWcHWmw1ZuCzphgd5KfWGJ",
     "wyvPkWjVZz1M8fHQnMMCDTQDbkManefNNhweYk5WkcF",
+    "3KCKozbAaF75qEU33jtzozcJ29yJuaLJTy2jFdzUY8bT",
     "4vieeGHPYPG2MmyPRcYjdiDmmhN3ww7hsFNap8pVN3Ey",
     "4TQLFNWK8AovT1gFvda5jfw2oJeRMKEmw7aH6MGBJ3or",
-    "4ACfpUFoaSD9bfPdeu6DBt89gB6ENTeHBXCAi87NhDEE",
-    "D1Mc6j9xQWgR1o1Z7yU5nVVXFQiAYx7FG9AW1aVfwrUM",
-    "9bnz4RShgq1hAnLnZbP8kbgBg1kEmcJBYQq3gQbmnSta",
-    "3KCKozbAaF75qEU33jtzozcJ29yJuaLJTy2jFdzUY8bT",
 ];
 
 /// Helius fast sender endpoint
-pub const SENDER_ENDPOINT: &str = "https://sender.helius-rpc.com/fast";
+/// Use Frankfurt regional endpoint for lower latency (backend applications)
+pub const SENDER_ENDPOINT: &str = "http://fra-sender.helius-rpc.com/fast";
 
 /// Helper function to get Pump Program ID as Pubkey
 pub fn pump_program_id() -> Pubkey {
